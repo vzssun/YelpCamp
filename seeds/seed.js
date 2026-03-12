beaff22fae4d6ceef04764d760070bc2d5d7f1b8
vzssun <vzssun@outlook.com>
2026-03-12 13:52:55 -0300

    Refactor: Changed the way we seed our db
    - Now first we push our camps to an array and then later we insert it.

--- seeds/seed.js
+++ seeds/seed.js
@@ -9,18 +9,19 @@ const seedDB = async () => {
 };
 
 seedDB().then(async () => {
-  Campground.deleteMany({});
+  await Campground.deleteMany({});
+  const camps = [];
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
-    const camp = new Campground({
+    camps.push({
       title: `${descriptors[Math.floor(Math.random() * descriptors.length)]} ${places[Math.floor(Math.random() * places.length)]}`,
       price,
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, eaque.",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
     });
-    await camp.save();
   }
+  await Campground.insertMany(camps);
   console.log("Database seeded");
   process.exit();
 });
